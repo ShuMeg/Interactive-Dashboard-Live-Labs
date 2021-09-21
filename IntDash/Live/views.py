a1d290aa71353b9dd2db88af894d5dcdd21a5a98
ShuMeg <65118607+ShuMeg@users.noreply.github.com>
2021-09-21 21:45:24 +0200

    view updates

---
+++ IntDash/Live/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def room1_display(request):
+    return render(request, 'room1.html')
+    
+def room2_display(request):
+    return render(request, 'room2.html')
+    
