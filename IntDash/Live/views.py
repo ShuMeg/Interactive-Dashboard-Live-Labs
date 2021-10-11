b20ed5bfbd592a7874743c8c00c2363c7511c05d
ShuMeg <meghna4896@gmail.com>
2021-10-11 21:35:52 +0200

    actor

--- IntDash/Live/views.py
+++ IntDash/Live/views.py
@@ -1,28 +1,51 @@
 import json
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import StreamingHttpResponse
 from django.http import JsonResponse
 
 
+headers = {
+    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIzYTBhZWIwZjVmZjE0MzkwYWE4YjVlN2ZmYTg0NDU4MiIsImlhdCI6MTYzMzk1OTg1OCwiZXhwIjoxOTQ5MzE5ODU4fQ.yXJe76bpJOUPsJB8YQTQzWkKmnSBf7BXlrQHc99AMYA",
+    "content-type": "application/json",
+}
+
+home-assistant = "http://localhost:8123/api/services/"
+
 # Create your views here.
 def room1_display(request):
     return render(request, 'room1.html')
-    
-def room2_display(request):
-    return render(request, 'room2.html')
 
-def requestroom(request):
-    return render(request, 'room1.html')
     
 def getSensorData(request):
     print("in get sensor data")
-    file_name = "Files/" + "sunlight_sensor" + ".txt"
-    data = open(file_name, "r").readline()
-    f = {"sunlight_sensor": data}
+    file_sunlight_sensor = "Files/" + "sunlight_sensor" + ".txt"
+    file_temperature_sensor = "Files/" + "temperature_sensor" + ".txt"
+    
+    data_sunlight_sensor = open(file_sunlight_sensor, "r").readline()
+    data_temperature_sensor = open(file_temperature_sensor, "r").readline()
+    
+    f = {"sunlight_sensor": data_sunlight_sensor, "temperature_sensor" : data_temperature_sensor}
     print(f)
     return JsonResponse(json.dumps(f, ensure_ascii=False), safe=False)
     
+    
+def sendActorDataRoomLight(request):
+    url_roomlight = url + "input_boolean/toggle"
+    entity_data = {"entity_id" : "input_boolean.room_light" }
+    
+    response =  requests.post(url_roomlight, headers=headers, data = json.dumps(entity_data))
+    print (response.text)
+    return render(request, 'room1.html')
+    
+def sendActorDataWashLight(request):
+    url_washlight = url + "input_boolean/toggle"
+    entity_data = {"entity_id" : "input_boolean.wash_light" }
+    
+    response =  requests.post(url_washlight, headers=headers, data = json.dumps(entity_data))
+    print (response.text)
+    return render(request, 'room1.html')
     
     
 
